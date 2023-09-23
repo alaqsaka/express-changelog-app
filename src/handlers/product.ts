574da940b1fb53adc66f24ca74e75f068d495c39
alaqsaka <aqsaabidin15@gmail.com>
2023-09-23 22:25:54 +0700

    get products, get produtct

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,1 +1,29 @@
-export const updateProductHandler () => {}
+import prisma from "../db";
+
+// Get All
+export const getProducts = async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: req.user.id
+        },
+        include: {
+            products: true
+        }
+    })
+
+    res.json({data: user?.products});
+};
+
+// Get One
+export const getOneProduct = async (req, res) => {
+    const id = req.params.id;
+
+    const product = await prisma.product.findFirst({
+        where: {
+            id,
+            belongsToId: req.user.id
+        }
+    });
+
+    res.json({data: product});
+}
